1cdcd721347215aeb9294f0e18dc4634545d9d0e
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-03-15 20:32:48 +0200

    install tailwindcss

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Watch & Learn</title>
+</head>
+<body>
+    <h1 class="text-3xl font-bold underline text-violet-600">
+        Hello world!
+    </h1>
+</body>
+</html>
