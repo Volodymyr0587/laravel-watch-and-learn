b841e69466949c0ea8c31c0d85841266d32a3f48
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-03-18 15:23:19 +0200

    Showing course by ID

--- routes/web.php
+++ routes/web.php
@@ -5,24 +5,28 @@ use Illuminate\Support\Facades\Route;
 
 $courses = [
     [
+        'id' => 0,
         'title' => 'Course 1',
         'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam laudantium nesciunt iure dolorum',
         'lessons_count' => 21,
         'length' => '2h 30m',
     ],
     [
+        'id' => 1,
         'title' => 'Course 2',
         'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam laudantium nesciunt iure dolorum',
         'lessons_count' => 16,
         'length' => '1h 50m',
     ],
     [
+        'id' => 2,
         'title' => 'Course 3',
         'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam laudantium nesciunt iure dolorum',
         'lessons_count' => 12,
         'length' => '1h 30m',
     ],
     [
+        'id' => 3,
         'title' => 'Course 4',
         'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam laudantium nesciunt iure dolorum',
         'lessons_count' => 32,
@@ -35,7 +39,7 @@ Route::get('/', fn () => view('index', ['courses' => $courses]))->name('index');
 
 Route::get('/courses', fn () => view('courses', ['courses' => $courses]))->name('courses');
 
-Route::get('/course', fn () => view('course', ['course' => $courses[0]]))->name('course');
+Route::get("/course/{id}", fn ($id) => view('course', ['course' => $courses[$id]]))->name('course');
 
 
 Route::view('/contact', 'contact')->name('contact');
