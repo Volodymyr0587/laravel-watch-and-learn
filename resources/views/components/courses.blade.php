4741f5764e3bfdcd6fbf5d450d4225842774dce8
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-03-18 14:19:28 +0200

    Course Component

--- resources/views/components/courses.blade.php
+++ resources/views/components/courses.blade.php
@@ -1,27 +1,38 @@
+@php
+$courses = [
+    [
+        'title' => 'Course 1',
+        'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam laudantium nesciunt iure dolorum',
+        'lessons_count' => 21,
+        'length' => '2h 30m',
+    ],
+    [
+        'title' => 'Course 2',
+        'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam laudantium nesciunt iure dolorum',
+        'lessons_count' => 16,
+        'length' => '1h 50m',
+    ],
+    [
+        'title' => 'Course 3',
+        'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam laudantium nesciunt iure dolorum',
+        'lessons_count' => 12,
+        'length' => '1h 30m',
+    ],
+    [
+        'title' => 'Course 4',
+        'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam laudantium nesciunt iure dolorum',
+        'lessons_count' => 32,
+        'length' => '3h 45m',
+    ],
+];
+@endphp
+
 <section class="w-full">
     <div class="container flex flex-col gap-8 pb-24 px-4">
         <h2 class="text-center font-black text-4xl">Latest Courses</h2>
         <div class="grid grid-cols-2 gap-4">
-            @foreach (range(0, 3) as $i)
-            <a href="{{ route('course') }}" class="rounded-md bg-gray-100 hover:bg-gray-200">
-                <article class="p-4">
-                    <header>
-                        <h3 class="font-semibold text-xl">Lorem, ipsum dolor.</h3>
-                    </header>
-                    <p class="font-normal mb-8 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque,
-                        quas.</p>
-                    <footer class="flex gap-2 justify-between mt-auto">
-                        <div class="flex items-center gap-2">
-                            <x-icon name="film" class="size-4"/>
-                            <span class="text-sm font-semibold">23 lessons</span>
-                        </div>
-                        <div class="flex items-center gap-2">
-                            <x-icon name="clock" class="size-4"/>
-                            <span class="text-sm font-semibold">2h 30m</span>
-                        </div>
-                    </footer>
-                </article>
-            </a>
+            @foreach ($courses as $course)
+            <x-course :$course />
             @endforeach
         </div>
     </div>
