4634fac2d1a9a13ed4fc515bcbc7627492af7c0f
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-03-18 21:22:59 +0200

    Inspect SQL queries

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //% Inspect SQL queries
+        // DB::listen(function (QueryExecuted $query) {
+        //     dump([$query->sql, $query->bindings, $query->time]);
+        // });
     }
 }
