22a8d393fa0162ef6830c1dd5ab3355645dfdc29
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-03-18 18:51:50 +0200

    Retrieving courses from the database

--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,25 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    $courses = DB::select("SELECT * FROM courses");
 
-$courses = [
-    [
-        'id' => 0,
-        'title' => 'Course 1',
-        'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam laudantium nesciunt iure dolorum',
-        'lessons_count' => 21,
-        'length' => '2h 30m',
-    ],
-    [
-        'id' => 1,
-        'title' => 'Course 2',
-        'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam laudantium nesciunt iure dolorum',
-        'lessons_count' => 16,
-        'length' => '1h 50m',
-    ],
-    [
-        'id' => 2,
-        'title' => 'Course 3',
-        'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam laudantium nesciunt iure dolorum',
-        'lessons_count' => 12,
-        'length' => '1h 30m',
-    ],
-    [
-        'id' => 3,
-        'title' => 'Course 4',
-        'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam laudantium nesciunt iure dolorum',
-        'lessons_count' => 32,
-        'length' => '3h 45m',
-    ],
-];
+    return view('index', compact('courses'));
+})->name('index');
 
+Route::get('/courses', function () {
+    $courses = DB::select("SELECT * FROM courses");
 
-Route::get('/', fn () => view('index', ['courses' => $courses]))->name('index');
+    return view('courses', compact('courses'));
+})->name('courses');
 
-Route::get('/courses', fn () => view('courses', ['courses' => $courses]))->name('courses');
+Route::get("/course/{id}", function ($id) {
+    $course = DB::selectOne("SELECT * FROM courses WHERE id = :id", compact('id'));
 
-Route::get("/course/{id}", fn ($id) => view('course', ['course' => $courses[$id]]))->name('course');
+    return view('course', compact('course'));
+})->name('course');
 
 
 Route::view('/contact', 'contact')->name('contact');
