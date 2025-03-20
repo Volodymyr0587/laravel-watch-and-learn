cc63bdd9584ff19740e396583207aed0876af93c
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-03-20 17:46:25 +0200

    add @tailwindcss/forms plugin

--- resources/views/components/course-search.blade.php
+++ resources/views/components/course-search.blade.php
@@ -3,7 +3,7 @@
         <div class="flex-grow">
             <input type="search"
                     placeholder="Course title or description..."
-                    class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500 focus:border-violet-500">
+                    class="w-full rounded-md border-gray-300 focus:border-violet-300 focus:ring focus:ring-violet-500 focus:ring-opacity-50">
         </div>
         <button type="submit" class="rounded-md px-4 bg-violet-500 hover:bg-violet-600 hover:cursor-pointer">
             <x-icon name="magnifying-glass" class="size-6 fill-white" />
