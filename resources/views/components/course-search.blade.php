abf4fc918e27940771c2363b00dd6b09c805b496
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-03-20 16:02:46 +0200

    search form

---
+++ resources/views/components/course-search.blade.php
@@ -0,0 +1,12 @@
+<form action="">
+    <div class="flex gap-2">
+        <div class="flex-grow">
+            <input type="search"
+                    placeholder="Course title or description..."
+                    class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500 focus:border-violet-500">
+        </div>
+        <button type="submit" class="rounded-md px-4 bg-violet-500 hover:bg-violet-600">
+            <x-icon name="magnifying-glass" class="size-6 fill-white" />
+        </button>
+    </div>
+</form>
--- resources/views/pages/courses/index.blade.php
+++ resources/views/pages/courses/index.blade.php
@@ -1,7 +1,8 @@
 <x-layout>
     <section class="pt-32 px-10">
         <div class="container flex flex-col gap-8 pb-24">
-            <h2 class="text-center font-black text-4xl">Courses</h2>
+            <h2 class="text-center font-extrabold text-5xl">Courses</h2>
+            <x-course-search />
             <div class="grid grid-cols-2 gap-4">
                 @foreach ($courses as $course)
                 <x-course :$course />
