2e083f5ba37f19708ac3f605b6325a244d056134
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-03-20 16:07:21 +0200

    add hover:cursor-pointer to search button

--- resources/views/components/course-search.blade.php
+++ resources/views/components/course-search.blade.php
@@ -5,7 +5,7 @@
                     placeholder="Course title or description..."
                     class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500 focus:border-violet-500">
         </div>
-        <button type="submit" class="rounded-md px-4 bg-violet-500 hover:bg-violet-600">
+        <button type="submit" class="rounded-md px-4 bg-violet-500 hover:bg-violet-600 hover:cursor-pointer">
             <x-icon name="magnifying-glass" class="size-6 fill-white" />
         </button>
     </div>
