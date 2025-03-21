ec0e464f78141c35428f6e665a511be4403aa3bc
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-03-21 15:03:55 +0200

    Filling lessons table (model, migration, seeder, factory)

---
+++ database/seeders/LessonSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Lesson;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LessonSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Lesson::truncate(); // delete all from lessons table before seed data
+
+        Lesson::factory(200)->create();
+    }
+}
