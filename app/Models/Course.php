7fd81e0872872191cd5b9aa0dd482a04448b4c96
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-03-22 19:43:34 +0200

    Has One of Many: get first lesson of course

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -32,5 +33,17 @@ class Course extends Model
     public function lessons(): HasMany
     {
         return $this->hasMany(Lesson::class);
+    }
+
+
+    /**
+     * Get the firstLesson associated with the Lesson
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function firstLesson(): HasOne
+    {
+        // return $this->hasOne(Lesson::class)->ofMany('number', 'min');
+        return $this->lessons()->one()->ofMany('number', 'min');
     }
 }
