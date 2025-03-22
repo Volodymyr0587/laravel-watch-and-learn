13164a76aa219d9dfcc7db42c41583238600cec6
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-03-22 16:27:59 +0200

    Change video url in player component

--- resources/views/components/lesson/player.blade.php
+++ resources/views/components/lesson/player.blade.php
@@ -1,5 +1,5 @@
 <iframe class="aspect-video rounded-md"
-    src="https://www.youtube.com/embed/hel0GmxeHyI?list=PLnKfPkeIekbb7X0TqmNNdX-CKOJaYNTpu"
+    src="https://www.youtube.com/embed/3JBmbQsR0ag?list=PL4cUxeGkcC9iMeu6v8Y-bxhtIOzYcB2Ot"
     title="{{ $lesson->title }}"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
