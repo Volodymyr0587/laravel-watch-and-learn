8f230e8bcb6a369d0e727c160dab4b18d1adef39
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-03-22 20:12:44 +0200

    Lessons navigation (Accessors)

--- resources/views/pages/lessons/show.blade.php
+++ resources/views/pages/lessons/show.blade.php
@@ -5,13 +5,19 @@
             <div class="flex flex-col gap-2">
                 <x-lesson.player :$lesson />
                 <div class="flex gap-2">
+                    <div class="flex gap-2 mr-auto">
+                        @if ($lesson->previous)
+                            <a href="{{ route('lessons.show', $lesson->previous) }}" class="rounded-md px-4 py-2 bg-gray-100 hover:bg-gray-200">
+                                <x-icon name="chevron-left" class="size-6" />
+                            </a>
+                        @endif
 
-                    <a href="#" class="rounded-md px-4 py-2 bg-gray-100 hover:bg-gray-200">
-                        <x-icon name="chevron-left" class="size-6" />
-                    </a>
-                    <a href="#" class="mr-auto rounded-md px-4 py-2 bg-gray-100 hover:bg-gray-200">
-                        <x-icon name="chevron-right" class="size-6" />
-                    </a>
+                        @if ($lesson->next)
+                            <a href="{{ route('lessons.show', $lesson->next) }}" class="rounded-md px-4 py-2 bg-gray-100 hover:bg-gray-200">
+                                <x-icon name="chevron-right" class="size-6" />
+                            </a>
+                        @endif
+                    </div>
 
                     <a href="{{ route('courses.show', $lesson->course) }}"
                         class="font-semibold rounded-md px-4 py-2 bg-gray-100 hover:bg-gray-200">
